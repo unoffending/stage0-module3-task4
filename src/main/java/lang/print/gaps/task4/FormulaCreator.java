4460b504af61cd4892025515ec9bddc426f07b15
unoffending <109032978+unoffending@users.noreply.github.com>
2023-05-18 17:11:04 +0600

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        int formula = (2 * a * 2 * a) - (5 * b) + 2 + a - 7 * ((a + b - 4 * a * b)/2);
     }
 }
